101fd08c5fddbb1c0e521b7df6da8c55af051e72
gaidai097 <42537902+gaidai097@users.noreply.github.com>
2018-12-14 12:22:05 +0200

    first commit with lightning components

---
+++ Sites/aura/Task9_Lightning/Task9_Lightning.app
@@ -0,0 +1,14 @@
+<aura:application extends="force:slds" implements="force:appHostable,flexipage:availableForAllPageTypes,flexipage:availableForRecordHome,force:hasRecordId,forceCommunity:availableForAllPageTypes">
+  
+    <div class="slds-grid slds-m-top_large">
+        <div class="slds-col slds-size_2-of-12">
+            
+        </div>
+        <div class="slds-col slds-size_8-of-12 slds-box">
+            <c:JobAdvertisementList />
+        </div>
+        <div class="slds-col slds-size_2-of-12">
+            
+        </div>
+    </div>
+</aura:application>
---
+++ force-app/main/default/aura/JobAdvertisementList/JobAdvertisementListController.js
@@ -0,0 +1,18 @@
+({
+	doInit: function(component, event, helper) {
+        // Create the action
+        var action = component.get("c.getJobs");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.jobs", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    }
+})
