c95f67e629f75d75cb272943b2c1994a0a90f81a
gaidai097 <42537902+gaidai097@users.noreply.github.com>
2018-12-27 19:33:10 +0200

    updated date filter

--- force-app/main/default/aura/FilterBar/FilterBarController.js
+++ force-app/main/default/aura/FilterBar/FilterBarController.js
@@ -2,8 +2,7 @@
 	refilter : function(component, event, helper) {        
         helper.refilterPage(component );
 	},
-    handleSubmit : function(component, event, helper) {  
-        
+    handleSubmit : function(component, event, helper) {
         if (event.keyCode === 13) {
           console.log('event.keyCode => ' + event.keyCode );
             helper.refilterPage(component );
@@ -11,7 +10,7 @@
 	},
     clearFilters : function(component, event, helper) {        
         component.find('nameFilter').set("v.value", null);
-        component.find('dateFilter').set("v.value", 'MORE');
+        component.find('dateFilter').set("v.value", 'ALL');
         component.find('publishedDate').set("v.value", null);
         component.find('salaryParam').set("v.value", null);
         component.find('salaryFilter').set("v.value", 'MORE');
@@ -21,8 +20,7 @@
         var selectedMenuItemValue = event.getParam("value");
         console.log(' Selected => ' + selectedMenuItemValue);
         component.set("v.dateFilter", selectedMenuItemValue);
-        component.find('dateFilter').set("v.value", selectedMenuItemValue);
-        
+        component.find('dateFilter').set("v.value", selectedMenuItemValue);        
     },
     selectSalaryFilter: function(component, event, helper) {
         var selectedMenuItemValue = event.getParam("value");
