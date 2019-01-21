ef348a960d5f14c6d5ae9509222543af073e942e
gaidai097 <42537902+gaidai097@users.noreply.github.com>
2019-01-21 16:54:18 +0200

    Double drag and Drop Final

--- force-app/main/default/aura/modalCV/modalCVController.js
+++ force-app/main/default/aura/modalCV/modalCVController.js
@@ -51,8 +51,12 @@
     },
     handleUploadFinished: function (component, event, helper) {   
         
-        var uploadedFiles = event.getParam("files");
-        console.log(uploadedFiles.length + ' ' + uploadedFiles[0].name);        
+        var files = event.getParam("files");
+        if (files.length>1) {
+            return alert("You can upload only one image");
+        }
+        console.log(files[0].name + ' ' + files[0].size);
+        helper.validateUploadedfile(component, event, files[0]);
     },
     onDragOver: function(component, event) {
         
@@ -80,65 +84,7 @@
         if (files.length>1) {
             return alert("You can upload only one image");
         }
-        console.log(files[0].name + ' ' + files[0].size);
-        
-        
-        
-        var file = files[0];             
-        if(file.size > 128*1024){
-            inputFile.value = "";
-            alert('File is too big',' Choose only image less then 128Kb !');
-        }else{
-            var marker = false;            
-            var types = ["image/gif","image/jpeg","image/jpg","image/png","image/bmp"];                
-            for(var j = 0; j< types.length; j++){
-                if(types[j] == file.type){
-                    marker = true;
-                    break; 
-                };
-            };
-            if(marker){                 
-                var fileSize = file.size;
-                if (file.size > 1024 * 1024) {
-                    fileSize = (Math.round(file.size*100 / (1024 * 1024)) /100).toString() + ' MB';
-                }else {
-                    fileSize = (Math.round(file.size / 1024) ).toString() + ' KB';
-                };                
-   				var imageName = file.name;
-                if(file.name.length >20){
-                    imageName = file.name.substring(0, 20) + "...";
-                };                
-                var reader = new FileReader();                
-                reader.onload = function (e) {
-                    var tmppath =  e.target.result;
-                    var removeLink = "<a class=\"removeFile\" href=\"#\" >X</a>";
-
-                    var content = "<span id=\"imageName\"><strong>" + imageName + "</strong> -<span> "+ fileSize +"<span>&nbsp; &nbsp; "+ removeLink + "<img alt='Your image' class='preview_img' src='"+tmppath+"'></img></span>"
-                    document.getElementById("preview").innerHTML += content;
-                    component.set('v.cvPhoto', tmppath);
-                    
-                    document.getElementById("imageName").addEventListener("mouseout", function( event ) { 
-                        document.getElementsByClassName("preview_img")[0].style.display = 'none';
-                        console.log('out');
-                    });
-                    document.getElementById("imageName").addEventListener("mouseover", function( event ) { 
-                        console.log('on');
-                        document.getElementsByClassName("preview_img")[0].style.display = 'block'; 
-                    });
-                }
-                reader.readAsDataURL(file);
-                
-                $A.util.removeClass( component.find('draggableDiv') , 'on_drag');
-                
-                
-            }else{ 
-                alert('Inappropriate type. Choose only image file!'); 
-            }                
-        }       
-        
-        
-        
-        
+        helper.validateUploadedfile(component, event, files[0]);
         
 	}
     
--- force-app/main/default/aura/modalCV/modalCVHelper.js
+++ force-app/main/default/aura/modalCV/modalCVHelper.js
@@ -1,5 +1,57 @@
 ({
-	helperMethod : function() {
-		
-	}
+    validateUploadedfile : function(component, event, file) {
+        if(file.size > 128*1024){
+            inputFile.value = "";
+            alert('File is too big',' Choose only image less then 128Kb !');
+        }else{
+            var marker = false;            
+            var types = ["image/gif","image/jpeg","image/jpg","image/png","image/bmp"];                
+            for(var j = 0; j< types.length; j++){
+                if(types[j] == file.type){
+                    marker = true;
+                    break; 
+                };
+            };
+            if(marker){                 
+                var fileSize = file.size;
+                if (file.size > 1024 * 1024) {
+                    fileSize = (Math.round(file.size*100 / (1024 * 1024)) /100).toString() + ' MB';
+                }else {
+                    fileSize = (Math.round(file.size / 1024) ).toString() + ' KB';
+                };                
+                var imageName = file.name;
+                if(file.name.length >20){
+                    imageName = file.name.substring(0, 20) + "...";
+                };                
+                var reader = new FileReader();                
+                reader.onload = function (e) {
+                    var tmppath =  e.target.result;
+                    var removeLink = "<span class=\"removeFile\" title=\"Remove\" href=\"#\" ><span>&#10006;</span></span>";
+                    
+                    var content = "<span id=\"imageName\"><strong>" + imageName + "</strong> -<span> "+ fileSize +"<span>&nbsp; &nbsp; "+ removeLink + "<img alt='Your image' class='preview_img' src='"+tmppath+"'></img></span>"
+                    document.getElementById("preview").innerHTML += content;
+                    component.set('v.cvPhoto', tmppath);
+                    document.getElementsByClassName("dragPicture")[0].classList.toggle('toggle'); 
+                    $A.util.addClass( component.find('inputFile'), 'toggle');
+                    document.getElementById("imageName").addEventListener("mouseout", function( event ) { 
+                        document.getElementsByClassName("preview_img")[0].style.display = 'none';
+                    });
+                    document.getElementById("imageName").addEventListener("mouseover", function( event ) { 
+                        document.getElementsByClassName("preview_img")[0].style.display = 'block'; 
+                    });
+                    document.getElementsByClassName("removeFile")[0].addEventListener("click", function( event ) { 
+                        $A.util.removeClass( component.find('inputFile'), 'toggle');
+                        document.getElementsByClassName("dragPicture")[0].classList.toggle('toggle');                        
+                        document.getElementById("imageName").remove();
+                    });
+                }
+                reader.readAsDataURL(file);                
+                $A.util.removeClass( component.find('draggableDiv') , 'on_drag');
+                
+                
+            }else{ 
+                alert('Inappropriate type. Choose only image file!'); 
+            }                
+        }       
+    }
 })
